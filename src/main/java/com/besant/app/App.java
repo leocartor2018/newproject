6b1aa91de77864ceefc390de5fa3c48eb67db0ec
leocartor2018 <leo.cartor@gmail.com>
2018-07-27 11:49:47 +0530

    Adding files

---
+++ src/main/java/com/besant/app/App.java
@@ -0,0 +1,13 @@
+package com.besant.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
